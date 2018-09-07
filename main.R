05e19c03d2ad4a091943628b6530863b6a026578
Rebecca Powell <Rebecca.L.Powell@du.edu>
2018-09-06 21:52:12 -0600

    Update function names in vignette, main.R

--- main.R
+++ main.R
@@ -1,72 +1,84 @@
-# grassmapr: Example Script - COlorado
+# grassmapr: Example Script - North America
 
-# Load climate data
-data(COMeanTmp) # mean monthly temperature (deg. C)
-data(COMeanPre) # mean monthly precipitation (mm)
+# Load North America climate data
+data(temp_NA) # mean monthly temperature (deg. C)
+data(precip_NA) # mean monthly precipitation (mm)
 
-# Growing season precipitation mask (>= 25 mm)
-precip_mask <- maskClimateVals(COMeanPre, 25)
-# Growing season temperature mask (>= 5 deg. C)
-GS_temp_mask <- maskClimateVals(COMeanTmp, 5)
-# C4 temperature mask (>= 22 deg. C)
-C4_temp_mask <- maskClimateVals(COMeanTmp, 22)
+# Set a C4 temperature threshold based on the COT model (>= 22 deg. C)
+C4_temp <- 22
+# Set a growing season temperature threshold (>= 5 deg. C)
+GS_temp <- 5
+# Set a precipitation threshold (>= 25 mm)
+min_prec <- 25
 
 # Generate Growing Season (GS) climate masks
-GS_mask <- combineMasks(GS_temp_mask, precip_mask)
+GS_masks <- combineMasks(GS_temp_mask, precip_mask)
 # Generate C4 climate masks
-C4_mask <- combineMasks(C4_temp_mask, precip_mask)
+C4_masks <- combineMasks(C4_temp_mask, precip_mask)
+
+# Generate Growing Season (GS) climate masks
+GS_masks <- mask_climate(temp.stack = temp_NA,
+  temp.threshold = GS_temp,
+  precip.stack = precip_NA,
+  precip.threshold = min_prec)
+
+# Generate C4 climate masks
+C4_masks <- mask_climate(temp.stack = temp_NA,
+  temp.threshold = C4_temp,
+  precip.stack = precip_NA,
+  precip.threshold = min_prec)
 
 # Optional - count number of months that satisfy each climate criteria
-GS_month_total <- countMonths(GS_mask)
-C4_month_total <- countMonths(C4_mask)
+GS_month_total <- countMonths(GS_masks)
+C4_month_total <- countMonths(C4_masks)
 
 # Remove intermediate raster objects
-rm(COMeanPre, COMeanTmp)
-rm(precip_mask, C4_temp_mask, GS_temp_mask)
+rm(precip_NA, temp_NA)
 
 # Load monthly NDVI layers
-data(COMonthlyNDVI)
+data(ndvi_NA)
 
 # Calculate C4 ratio based on C4 climate only
-C4_ratio <- calcC4Ratio(C4_mask, GS_mask)
+C4_ratio <- calc_C4_ratio(C4_masks, GS_masks)
 
 #OR Calculate C4 ratio based on C4 climate AND vegetation productivity
-C4_ratio_vi <- calcC4Ratio(C4_mask, GS_mask, veg.index = COMonthlyNDVI)
+C4_ratio_vi <- calc_C4_ratio(C4_masks, GS_masks, veg.index = ndvi_NA)
 
 # Remove intermediate raster objects
-rm(GS_mask, C4_mask, COMonthlyNDVI)
+rm(GS_masks, C4_masks, ndvi_NA)
 
 # Load non-grass vegetation layers
-data(COWoody)
-data(COC3Crop)
-data(COC4Crop)
-
-# Downsample C4_ratio to match finer resolution of vegetation layers
-C4_ratio_rs <- resample(x = C4_ratio, y = COWoody, method = "ngb")
+data(woody_NA)
+data(cropC3_NA)
+data(cropC4_NA)
 
 # Create raster stack of other (non-grassy) vegetation layers
-veg_layers <- stack(COC4Crop, COC3Crop, COWoody)
+veg_layers <- stack(woody_NA, cropC3_NA, cropC4_NA)
 
 #Indicate layers that correspond to C4 vegetation
-C4_flag <- c(1, 0, 0)
+C4_flag <- c(0, 0, 1)
 
 # Indicate layers that correspond to herbaceous vegetation
-herb_flag <- c(1, 1, 0)
+herb_flag <- c(0, 1, 1)
 
-# Generate PFT vegetation cover brick
-pft_cover <- calcPFTCover(C4_ratio_rs, veg_layers, C4_flag, herb_flag)
+# Generate PFT vegetation cover brick (C4 grass, C3 grass, woody)
+pft_cover <- calc_pft_cover(C4.ratio = C4_ratio,
+                            GS.mask = GS_masks,
+                            veg.layers = veg_layers,
+                            C4.flag = C4_flag,
+                            herb.flag = herb_flag)
 
 # d13C endmember vector for PFT layers from the literature
 d13C_emb <- c(-12.5, -26.7, -27.0) # C4 herb, C3 herb, Woody
 
 # Apply mixing model to generate d13C isoscape
-d13C_iso <- calcDel13C(pft_cover, d13C_emb)
+d13C_iso <- calc_del13C(pft_cover, d13C_emb)
 
 # Standard deviations of d13C endmember means from the literature
 d13C_std <- c(1.1, 2.3, 1.7) # C4 herb, C3 herb, Woody
 
 # Calculate weighted standard deviation of mean d13C values
-d13C_iso_std <- calcDel13C(pft_cover, d13C_std)
+d13C_iso_std <- calc_del13C(pft_cover, d13C_std)
 
 # Plot d13C isoscape and standard deviation layers
 par(mfrow = c(1,2))
@@ -76,8 +88,8 @@ plot(d13C_iso_std, main = "Std. Dev. d13C \n(per mil)",
   xlab = "longitude", ylab = "latitude")
 
 # Remove intermediate raster objects
-rm(COWoody, COC3Crop, COC4Crop)
-rm(C4_ratio, C4_ratio_rs, veg_layers, pft_cover)
+rm(woody_NA, cropC3_NA, cropC4_NA)
+rm(C4_ratio, C4_ratio_vi, veg_layers, pft_cover)
 
 
 
